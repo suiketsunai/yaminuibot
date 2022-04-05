e1052f5e128e890680605a1b6a39d17e63f99baf
suiketsunai <nvrmndr.desu@yandex.ru>
2022-04-05 05:50:31 +0300

    Revert log directory, refine code

--- extra/loggers.py
+++ extra/loggers.py
@@ -2,12 +2,17 @@ import os
 import logging
 
 from pathlib import Path
+from datetime import datetime
 
 # working with env
 from dotenv import load_dotenv
 
 # reading setings
 import tomli
+
+# current timestamp & this file directory
+date_run = datetime.now()
+file_dir = Path(__file__).parent.parent
 
 ################################################################################
 # logger
@@ -27,13 +32,25 @@ logging.basicConfig(
 root_log = logging.getLogger()
 
 
-def get_file_handler():
+def get_file_handler() -> logging.FileHandler | None:
     """Create file handler"""
     file_log = config["log"]["file"]
     if file_log["enable"]:
         root_log.info("Logging to file enabled.")
-        log_name = "log.log"
-        log_file = Path(log_name)
+        log_dir = file_dir / file_log["path"]
+        if not log_dir.is_dir():
+            root_log.warning("Log directory doesn't exist.")
+            try:
+                root_log.info("Creating log directory...")
+                log_dir.mkdir()
+                root_log.info("Created log directory: '%s'.", log_dir.resolve())
+            except Exception as ex:
+                root_log.error("Exception occured: %s", ex)
+                root_log.info("Can't execute program.")
+                quit()
+        log_date = date_run.strftime(file_log["date"])
+        log_name = f'{file_log["pref"]}{log_date}.log'
+        log_file = log_dir / log_name
         root_log.info("Logging to file: '%s'.", log_name)
         # add file handler
         fh = logging.FileHandler(log_file, encoding="utf-8")
@@ -48,16 +65,22 @@ def get_file_handler():
 file_handler = get_file_handler()
 
 
-def setup_logging(name: str):
-    """Set up logger"""
-    log = logging.getLogger(name)
+def add_file_handler(logger: logging.Logger | str) -> None:
+    """Add file handler to logger
+
+    Args:
+        logger (str): logger or name of logger
+    """
+    if not logger:
+        root_log.error("No logger to add file handler to.")
+    if not isinstance(logger, logging.Logger):
+        logger = logging.getLogger(name)
     if file_handler:
-        log.addHandler(file_handler)
-    return log
+        logger.addHandler(file_handler)
 
 
 # setup root logger
-root_log = setup_logging("root")
+add_file_handler(root_log)
 
 # setup sqlalchemy loggers
 for name, module in config["log"]["sqlalchemy"].items():
