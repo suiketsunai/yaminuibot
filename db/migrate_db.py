a17864352fc54321814e7858e29c46eb2138c236
suiketsunai <nvrmndr.desu@yandex.ru>
2022-06-22 01:19:25 +0300

    Add new migration function

--- db/migrate_db.py
+++ db/migrate_db.py
@@ -4,6 +4,7 @@ import logging
 from pathlib import Path
 
 # working with database
+from sqlalchemy import func
 from sqlalchemy.orm import Session, aliased
 
 # working with timezone
@@ -16,7 +17,7 @@ from dateutil.parser import parse
 from db import engine
 
 # database models
-from db.models import User, Channel, ArtWork
+from db.models import User, Channel, Post, ArtWork
 
 # namedtuples
 from extra.namedtuples import Link
@@ -46,6 +47,94 @@ def check_message(message: dict) -> list[Link]:
 
 
 def migrate_db() -> None:
+    """Read exported jsons and insert data in database"""
+    src = Path(".dump")
+    log.info("Reading User and Channel json files...")
+    users = json.loads((src / "users.json").read_bytes())
+    channels = json.loads((src / "channels.json").read_bytes())
+    log.info("Done!")
+
+    log.info("Inserting Users and Channels to database...")
+    with Session(engine) as s:
+        for user in users:
+            forward_mode = user["forward_mode"]
+            user["forward_mode"] = False
+            s.add(User(**user))
+            user["forward_mode"] = user["forward_mode"]
+
+        for channel in channels:
+            s.add(Channel(**channel))
+
+        for user in users:
+            s.get(User, user["id"]).forward_mode = user["forward_mode"]
+        s.commit()
+    log.info("Done!")
+
+    log.info("Inserting Posts and ArtWorks to database...")
+    artposts = json.loads((src / "artworks.json").read_bytes())
+    with Session(engine) as s:
+        for artpost in artposts:
+            # adding artwork...
+            # check if it's already in database
+            if (
+                a := s.query(ArtWork)
+                .filter_by(aid=artpost["aid"])
+                .filter_by(type=artpost["type"])
+                .first()
+            ):
+                log.info("Found artwork: %s.", a.aid)
+                if artpost["files"] and not a.files:
+                    log.info("No-files artwork: %s.", a.aid)
+                    a.files = artpost["files"]
+            else:
+                a = ArtWork(
+                    aid=artpost["aid"],
+                    type=artpost["type"],
+                    files=artpost["files"],
+                )
+                s.add(a)
+                log.info("Added artwork: %s.", a.aid)
+            # adding post...
+            post_data = {
+                "artwork": a,
+                "channel_id": artpost["channel_id"],
+                "post_id": artpost["post_id"],
+                "post_date": artpost["post_date"],
+                "is_original": False if artpost["is_forwarded"] else True,
+                "is_forwarded": artpost["is_forwarded"],
+                "forwarded_channel_id": artpost["forwarded_channel_id"],
+            }
+            p = Post(**post_data)
+            s.add(p)
+            log.info("Added post: %s.", p.post_id)
+            # commit changes
+            s.commit()
+    log.info("Done!")
+
+    log.info("Finding all not first-posted ArtWorks...")
+    with Session(engine) as s:
+        for artwork in (
+            s.query(ArtWork)
+            .select_from(ArtWork)
+            .join(Post)
+            .group_by(ArtWork.id)
+            .having(func.count(ArtWork.posts) > 1)
+            .all()
+        ):
+            for post in artwork.posts[1:]:
+                if post.is_original:
+                    log.info(
+                        "%s: post %s from %s wasn't original.",
+                        artwork.aid,
+                        post.post_id,
+                        post.channel.name,
+                    )
+                post.is_original = False
+        s.commit()
+    log.info("Done!")
+
+
+def old_migrate_db() -> None:
     """Read exported jsons and insert data in database"""
     src = Path(".src")
     log.info("Reading User and Channel json files...")
